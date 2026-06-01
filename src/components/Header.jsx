4d47b8fc54c327b4ae6e6461bdba1b153f91b456
Busiswa Bili-bili <141039125+BUSISWABILIBILI@users.noreply.github.com>
2026-06-01 15:27:21 +0200

    feat: add game header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>Pokemon Memory Card</h1>
+      <p>
+        Click a Pokemon only once. Clicking the same Pokemon twice will reset
+        your score.
+      </p>
+    </header>
+  );
+}
