2d4fb230a90dab136890fe39d456b037aa39320e
Busiswa Bili-bili <141039125+BUSISWABILIBILI@users.noreply.github.com>
2026-06-01 15:14:56 +0200

    feat: create reusable pokemon card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,8 @@
+export default function Card({ pokemon, onClick }) {
+  return (
+    <div className="card" onClick={() => onClick(pokemon.id)}>
+      <img src={pokemon.image} alt={pokemon.name} />
+      <h3>{pokemon.name}</h3>
+    </div>
+  );
+}
