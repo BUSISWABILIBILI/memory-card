0722c2e70de0e51c147bde9f03d987294f5c92df
Busiswa Bili-bili <141039125+BUSISWABILIBILI@users.noreply.github.com>
2026-06-01 16:46:36 +0200

    Remove card number badges

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,10 @@
 export default function Card({ pokemon, onClick }) {
-  const pokemonNumber = String(pokemon.id).padStart(3, "0");
-
   return (
     <button
       className="card"
       onClick={() => onClick(pokemon.id)}
       type="button"
     >
-      <span className="card-number">#{pokemonNumber}</span>
       <span className="card-image-wrap">
         <img src={pokemon.image} alt="" />
       </span>
